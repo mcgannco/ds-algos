1085633588f4f4818ff1eacc4d15e960dc7e0d7a
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-08-13 18:03:10 -0400

    pracitce

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,28 +1,13 @@
-function ListNode(val) {
-  this.val = val;
-  this.next = null;
+function inPlaceReverse(arr) {
+  let startPointer = 0;
+  let endPointer = arr.length - 1;
+  while(startPointer < endPointer) {
+    let tmp = arr[startPointer];
+    arr[startPointer] = arr[endPointer];
+    arr[endPointer] = tmp
+
+    startPointer++;
+    endPointer--;
+  }
+  return arr;
 }
-
-var mergeTwoLists = function(l1, l2) {
-    let newLinkedList;
-    let currentNode;
-    let l1NodeList = l1;
-    let l2NodeList = l2;
-
-    while(l1NodeList.val && l2NodeList.val ) {
-
-    }
-};
-
-let nodeA = new ListNode(1);
-let nodeB = new ListNode(2);
-let nodeC = new ListNode(4);
-
-nodeA.next = nodeB;
-nodeB.next = nodeC;
-
-let nodeD = new ListNode(1);
-let nodeE = new ListNode(3);
-let nodeF = new ListNode(4);
-nodeD.next = nodeE;
-nodeE.next = nodeF;
