1cab24f71cb5727c88fff01b148e2bb86c04cb46
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-05 18:39:13 -0400

    practice

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,31 +1,15 @@
-function playlist(songs, k, q) {
-    let currentSong = songs[k];
-    let currentIdx = k;
-    let nextSong = songs[q];
-    let downMoves = 0;
-    let upMoves = 0;
+var reverseString = function(s) {
+  let arr = s.split("")
+    let pointer1 = 0;
+    let pointer2 = s.length - 1;
 
-    for(let i = k; i < songs.length + k; i++) {
-      let current = songs[i % songs.length]
-      if((current === nextSong) && (i != k)) {
-        break;
-      } else {
-        upMoves += 1
-      }
+    while(pointer1 < pointer2) {
+      const temp = arr[pointer1];
+      arr[pointer1] = arr[pointer2];
+      arr[pointer2] = temp;
+
+        pointer1++
+        pointer2--
     }
-
-    for(let i = k; i > -songs.length + k; i--) {
-      let idx = Math.abs(i % songs.length)
-      let current = songs[idx]
-      if((current === nextSong) && (i != k)) {
-        break;
-      } else {
-        downMoves += 1
-      }
-    }
-
-    return Math.min(upMoves, downMoves)
-
-}
-
-let songs = ["wheniseeyouagain", "borntorun", "nothingelsematters", "cecelia", 1, "cecelia"]
+    return arr.join("")
+};
