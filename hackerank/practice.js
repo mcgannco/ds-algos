1fe51142b1e12bfe1962659d6f371dacb613e85e
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-06 18:04:27 -0400

    practice

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,15 +1,1 @@
-var reverseString = function(s) {
-  let arr = s.split("")
-    let pointer1 = 0;
-    let pointer2 = s.length - 1;
 
-    while(pointer1 < pointer2) {
-      const temp = arr[pointer1];
-      arr[pointer1] = arr[pointer2];
-      arr[pointer2] = temp;
-
-        pointer1++
-        pointer2--
-    }
-    return arr.join("")
-};
