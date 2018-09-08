475146ba4b19d38be2d6ffc4dbd75fde9757a81d
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-07 20:13:22 -0400

    ll

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,60 +1,30 @@
-var spiralOrder = function(matrix) {
-    let len = matrix.length;
-    if(len === 0) {
-      return [];
-    }
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
 
-    let colLength = matrix[0].length;
-    let result = [];
-    let nums = colLength * len;
-    let currRow = 0;
-    let currCol = 0;
-    let rowMin = 0;
-    let rowMax = len - 1;
-    let colMin = 0;
-    let colMax = colLength - 1;
-    let numComplete = 0;
-    let h = 1;
-    let v = 0;
-    while(numComplete < nums) {
-      result.push(matrix[currRow][currCol]);
-      numComplete+= 1;
-      currRow += v;
-      currCol += h;
-
-      //upperRight
-      if(currCol === colMax + 1 && currRow === rowMin) {
-        rowMin += 1;
-        currRow+= 1;
-        currCol -=1;
-        h = 0;
-        v = 1;
-      }
-      //bottomRight
-      else if(currCol === colMax && currRow === rowMax + 1) {
-        colMax -=1;
-        currCol -=1;
-        currRow -=1;
-        h = -1;
-        v = 0;
-      }
-      //bottomLeft
-      else if(currCol === colMin - 1 && currRow === rowMax) {
-        currCol +=1;
-        currRow -=1;
-        rowMax -=1;
-        h = 0;
-        v = -1;
-      }
-      //topLeft
-      else if(currRow === rowMin - 1 && currCol === colMin) {
-        currRow+=1;
-        currCol+= 1;
-        colMin += 1;
-        h = 1;
-        v = 0;
-      }
+var isPalindrome = function(head) {
+    let listLength = 0;
+    let currentNode = head;
+    while(currentNode) {
+        listLength += 1;
+        currentNode = currentNode.next
     }
-    return result
+    let midPoint = listLength / 2;
+    reverseList(head, midPoint)
 };
-let input = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+var reverseList = function(head, len) {
+    let currentNode = head;
+    let prev = null;
+    let next = null;
+    while(len) {
+        nextNode = currentNode.next;
+        currentNode.next = prev;
+        prev = currentNode;
+        currentNode = nextNode
+        len -=1
+    }
+}
