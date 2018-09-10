06b09f09d15e16bbb18c5ffab42e54ebf57c14fe
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-10 19:05:15 -0400

    practice

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,49 +1,14 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null
+var alienOrder = function(words) {
+  let letterHash = {};
+  for(word in words) {
+    debugger
   }
-}
-
-let a = new ListNode(2)
-let b = new ListNode(3)
-let c = new ListNode(3)
-
-a.next = b
-
-var getIntersectionNode = function(headA, headB) {
-    if(!headA || !headB) return null
-    let [lenA, lenB] = [getLength(headA), getLength(headB)]
-    let [nodeA, nodeB] = [headA, headB]
-    let delta = Math.abs(lenA - lenB);
-    if(lenA > lenB) {
-        nodeA = moveAhead(nodeA, delta)
-    } else if(lenB > lenA) {
-        nodeB = moveAhead(nodeB, delta)
-    }
-
-    while(nodeA && nodeB) {
-        if(nodeA.val === nodeB.val) return nodeA
-        nodeA = nodeA.next
-        nodeB = nodeB.next
-    }
-    return null
 };
 
-function getLength(head) {
-    let len = 0, curr = head;
-    while(curr) {
-        len += 1
-        curr = curr.next
-    }
-    return len
-}
-
-function moveAhead(node, steps) {
-    let curr = node;
-    while(steps > 0) {
-        curr = curr.next
-        steps--
-    }
-    return curr
-}
+let input = [
+  "wrt",
+  "wrf",
+  "er",
+  "ett",
+  "rftt"
+];
