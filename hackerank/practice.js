a9f2735b54b5c00f6df4dcb74f689e356fbdf82a
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-11 20:09:22 -0400

    practice

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,14 +1,13 @@
-var alienOrder = function(words) {
-  let letterHash = {};
-  for(word in words) {
-    debugger
-  }
+var findKthLargest = function(nums, k) {
+    let tracker = 0
+    nums.sort(function(a, b) {
+      return a - b
+    });
+    
+    for(let i = nums.length - 1; i >= 0; i--) {
+        let num = nums[i];
+        tracker+=1
+        if(tracker === k) return num
+    }
 };
-
-let input = [
-  "wrt",
-  "wrf",
-  "er",
-  "ett",
-  "rftt"
-];
+let input = [3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6]
