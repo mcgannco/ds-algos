d9dffd46f22dbd4e9e3ed262b9ce1da1d2c06758
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-17 19:58:08 -0400

    practice

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,52 +1,17 @@
-
-
-function sudokuSolve(board, count) {
-  let r = -1;
-  let c = -1;
-  let candidates = null;
-
-  for(let row = 0; row < board.length; row++) {
-    for(let col = 0; col < board[row].length; col++) {
-      if(board[row][col] === ".") {
-        let newCandidates = getCandidates(board, row, col)
-        if(candidates === null || newCandidates.length < candidates.length) {
-          candidates = newCandidates
-          r = row
-          c = col
-        }
-      }
-    }
-  }
-
-  if(candidates === null) return true;
-  for(let i = 0; i < candidates.length; i++) {
-    board[r][c] = candidates[i];
-    if(sudokuSolve(board, count+=1)) {
+var plusOne = function(digits) {
+  debugger
+    let sum = 1;
+    for(let i = digits.length - 1; i >= 0; i--) {
       debugger
-      console.log(count)
-      return true
-    } else {
-      board[r][c] = "."
-    }
-  }
-  return false
-}
-
-function getCandidates(board, row,col) {
-  let candidates = [];
-  for(let i = 1; i < 10; i++) {
-    let option = i.toString()
-    let collision = false;
-    for(let i = 0; i < board.length; i++) {
-      if(board[row][i] === option || board[i][col] === option ||
-        board[(row - row % 3) + Math.floor(i / 3)][(col - col % 3) + i % 3] == option) {
-          collision = true;
-          break;
+        sum = sum + digits[i]
+        digits[i] = sum % 10
+        if(sum > 9) {
+            sum = 1
+        } else {
+            sum = 1
         }
     }
-    if(!collision) candidates.push(option)
-  }
-  return candidates
-}
 
-let input = [[".",".",".","7",".",".","3",".","1"],["3",".",".","9",".",".",".",".","."],[".","4",".","3","1",".","2",".","."],[".","6",".","4",".",".","5",".","."],[".",".",".",".",".",".",".",".","."],[".",".","1",".",".","8",".","4","."],[".",".","6",".","2","1",".","5","."],[".",".",".",".",".","9",".",".","8"],["8",".","5",".",".","4",".",".","."]]
+
+    return digits
+};
