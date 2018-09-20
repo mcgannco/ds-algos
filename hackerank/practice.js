e66487653b796fbc07e4d081628d938fb0d4ebe8
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-19 20:07:37 -0400

    stuff

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,17 +1,16 @@
-var plusOne = function(digits) {
-  debugger
-    let sum = 1;
-    for(let i = digits.length - 1; i >= 0; i--) {
-      debugger
-        sum = sum + digits[i]
-        digits[i] = sum % 10
-        if(sum > 9) {
-            sum = 1
+var merge = function(nums1, m, nums2, n) {
+    let[mainPointer, pointerOne, pointerTwo] = [m+n-1,m-1,n-1];
+    while(pointerTwo >= 0) {
+        if(nums1[pointerOne] && nums1[pointerOne] > nums2[pointerTwo]) {
+            nums1[mainPointer] = nums1[pointerOne]
+            pointerOne--
         } else {
-            sum = 1
+            nums1[mainPointer] = nums2[pointerTwo]
+            pointerTwo--
         }
+        mainPointer--
     }
-
-
-    return digits
 };
+
+let nums1 = [1,2,3,0,0,0], m = 3
+let nums2 = [2,5,6], n = 3
