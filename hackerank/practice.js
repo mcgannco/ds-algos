2a48f072fb4ac864196f35515f35cbbea21284c2
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-09-24 18:17:17 -0400

    practice

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,19 +1,29 @@
-var subsets = function(nums) {
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let a = new Node(1)
+let b = new Node(2)
+let c = new Node(3)
+a.right = b
+b.left = c
+
+var inorderTraversal = function(root) {
   debugger
-    var r = [];
-
-    if (nums.length == 0) r.push([]);
-    else {
-        r = subsets(nums.slice(0, nums.length - 1));
-
-        var len = r.length;
-
-        for(var i = 0; i < len; i++) {
-            var m = r[i].slice();
-            m.push(nums[nums.length - 1]);
-            r.push(m);
-        }
-    }
-
-    return r;
+    if(!root) return []
+    let final = []
+    helper(root, final)
+    return final
 };
+
+function helper(node, final) {
+  debugger
+    if(!node) return null
+    helper(node.left)
+    final.push(node.val)
+    helper(node.right)
+}
