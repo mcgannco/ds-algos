8dfb9fa5f2bc0d5979284f233a54fd8d731b1f41
Connor McGann <mcgann.27@buckeyemail.osu.edu>
2018-10-13 15:39:30 -0400

    practice

--- hackerank/practice.js
+++ hackerank/practice.js
@@ -1,40 +1,21 @@
-class Node {
+class TreeNode {
   constructor(val) {
-    this.val = val;
+    this.val = val
     this.left = null
     this.right = null
   }
 }
 
-let a = new Node(4)
-let b = new Node(2)
-let c = new Node(7)
-let d = new Node(1)
-let e = new Node(3)
-let f = new Node(6)
-let g = new Node(9)
-
-a.left = b
-a.right = c
-b.left = d
-b.right = e
-c.left = f
-c.right = g
-
-var levelOrder = function(root) {
-  debugger
-    let [final, queue] = [[],[root]];
-    if(!root) return final
-    while(queue.length) {
-        let sub = [];
-        let size = queue.length;
-        for(let i = 0; i < size; i++) {
-            let curr = queue.pop();
-            sub.push(curr.val)
-            if(curr.left){queue.unshift(curr.left)}
-            if(curr.right){queue.unshift(curr.right)}
-        }
-        final.push(sub)
-    }
-    return final
+var sortedArrayToBST = function(nums) {
+    return helper(nums, 0, nums.length - 1)
 };
+
+function helper(arr,start,end) {
+  debugger
+    if(start > end) return null
+    let mid = Math.floor(end - start) / 2;
+    let newNode = new TreeNode(arr[mid]);
+    newNode.left = helper(arr, start, mid-1)
+    newNode.right = helper(arr, mid+1, end)
+    return newNode
+}
